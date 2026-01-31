5c66d929882211a74a79f4ccdeedb0ac03777d03
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-01-31 18:40:33 +0100

    Added auth headers to https requests and Implemented custom exception classes

--- lib/service/promotion_service.dart
+++ lib/service/promotion_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:unitime/core/utils/exceptions.dart';
 import 'package:unitime/core/utils/result.dart';
 import 'package:unitime/data/promotion.dart';
 
@@ -11,10 +12,15 @@ class PromotionService {
 
   factory PromotionService() => _shared;
 
-  Future<Result<Promotion>> getPromotion(int promotionId) async {
+  Future<Result<Promotion>> getPromotion(
+    String accessCode,
+    String token,
+  ) async {
     try {
-      final res = await http.get(
+      final res = await http.post(
         Uri.parse("http://localhost:8080/api/promotions/get"),
+        body: jsonEncode(accessCode),
+        headers: {'Authorization': 'Bearer $token'}
       );
 
       if (res.statusCode == 200) {
@@ -28,41 +34,46 @@ class PromotionService {
           ),
         );
       } else {
-        return Result.error(Exception("Failed to get promotion."));
+        return Result.error(CouldNotGetPromotionException());
       }
     } on Exception catch (e) {
       return Result.error(e);
     }
   }
 
-  Future<Result<Promotion>> createPromotion(Promotion promotion) async {
+  Future<Result<Promotion>> createPromotion(
+    Promotion promotion,
+    String token,
+  ) async {
     try {
       final res = await http.post(
         Uri.parse("http://localhost:8080/api/promotions/create"),
         body: jsonEncode(promotion),
+        headers: {'Authorization': 'Bearer $token'}
       );
 
       if (res.statusCode == 200) {
         final promotion = jsonDecode(res.body);
         return Result.ok(Promotion.fromJson(promotion));
       } else {
-        return Result.error(Exception("Failed to create promotion"));
+        return Result.error(CouldNotCreatePromotionException());
       }
     } on Exception catch (e) {
       return Result.error(e);
     }
   }
 
-  Future<Result<void>> deletePromotion(String accessCode) async {
+  Future<Result<void>> deletePromotion(String accessCode, String token) async {
     try {
       final res = await http.delete(
         Uri.parse("http://localhost:8080/api/promotions/delete"),
+        headers: {'Authorization': 'Bearer $token'}
       );
 
       if (res.statusCode == 200) {
         return Result.ok(null);
       } else {
-        return Result.error(Exception("Failed to delete promotion"));
+        return Result.error(CouldNotDeletePromotionException());
       }
     } on Exception catch (e) {
       return Result.error(e);
