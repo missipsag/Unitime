e26102427dc8befe06d020c14965c76cd69430c0
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-02-28 23:46:40 +0100

    updated the color palette used accross the app

--- lib/core/constants/appColors.dart
+++ lib/core/constants/appColors.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFFA020F0);
+  AppColors._();
+
+  static const Color primaryColor = Color(0xFF52C488);
   static const Color secondaryColor = Color(0xFFE6CCFF);
   static const Color tertiaryColor = Color(0xFFFF5722);
-  static const Color surfaceColor = Color(0xFFFFFFFF);
-  static const Color backgroundColor = Color(0xFFF8F9FB);
-  static const Color errorColor = Color(0xFFB00020);
+  static const Color surfaceColor = Color(0xFFF7F7F7);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color errorColor = Color(0xFFE06969);
 }
