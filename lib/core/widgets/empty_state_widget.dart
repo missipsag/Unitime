a6f7f53ca78513c317a57964866f546a8c2fe3cd
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-03-13 11:59:18 +0100

    wrapped the whole widget in center, added text theme

--- lib/core/widgets/empty_state_widget.dart
+++ lib/core/widgets/empty_state_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:unitime/core/constants/app_spacing.dart';
 
 class EmptyStateWidget extends StatelessWidget {
   final String imagePath;
@@ -19,54 +20,56 @@ class EmptyStateWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // The SVG Illustration
-          SvgPicture.asset(
-            imagePath,
-            height: 200, // Keep illustrations uniform in size
-          ),
-          const SizedBox(height: 32),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // The SVG Illustration
+            SvgPicture.asset(
+              imagePath,
+              height: 100, // Keep illustrations uniform in size
+            ),
+            const SizedBox(height: TAppSpacing.md),
 
-          // The Headline
-          Text(
-            title,
-            style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 12),
-
-          // The Description
-          if (description != null && description!.isNotEmpty) ...[
+            // The Headline
             Text(
-              description!,
-              style: const TextStyle(fontSize: 16, color: Colors.grey),
+              title,
+              style: Theme.of(context).textTheme.bodyMedium,
               textAlign: TextAlign.center,
             ),
-          ],
-          const SizedBox(height: 32),
+            const SizedBox(height: TAppSpacing.sm),
 
-          // The Call-to-Action Button
-          if (onButtonPressed != null) ...[
-            ElevatedButton(
-              onPressed: onButtonPressed,
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(
-                  double.infinity,
-                  50,
-                ), // Full-width button
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
+            // The Description
+            if (description != null && description!.isNotEmpty) ...[
+              Text(
+                description!,
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.center,
               ),
-              child: Text(buttonText),
-            ),
+            ],
+            const SizedBox(height: TAppSpacing.sm),
+
+            // The Call-to-Action Button
+            if (onButtonPressed != null) ...[
+              ElevatedButton(
+                onPressed: onButtonPressed,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(
+                    double.infinity,
+                    50,
+                  ), // Full-width button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text(buttonText),
+              ),
+            ],
           ],
-        ],
+        ),
       ),
     );
   }
