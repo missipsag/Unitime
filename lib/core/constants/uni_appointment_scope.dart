9a6ca9a301db39b67686fc19bd37210af8819a3c
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-13 22:07:01 +0100

    added display name method for serializing purposes

--- lib/core/constants/uni_appointment_scope.dart
+++ lib/core/constants/uni_appointment_scope.dart
@@ -1,1 +1,13 @@
-enum UniAppointmentScope { PROMOTION, GROUP, GLOBAL }
+enum UniAppointmentScope {
+  PROMOTION,
+  GROUP,
+  GLOBAL;
+
+  String get displayName {
+    return switch (this) {
+      UniAppointmentScope.GLOBAL => "GLOBAL",
+      UniAppointmentScope.GROUP => "GROUP",
+      UniAppointmentScope.PROMOTION => "PROMOTION",
+    };
+  }
+}
