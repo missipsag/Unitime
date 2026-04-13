3f6c065a1486fd128cf532c037084f9926775e5a
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-13 22:54:05 +0100

    added robust error handling

--- lib/service/uni_appointment_service.dart
+++ lib/service/uni_appointment_service.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:unitime/core/constants/app_routes.dart';
+import 'package:unitime/core/utils/exceptions.dart';
 import 'package:unitime/core/utils/result.dart';
 import 'package:unitime/data/uni_appointment.dart';
 
@@ -15,11 +16,17 @@ class UniAppointmentService {
 
   factory UniAppointmentService() => _shared;
 
-  Future<Result<List<UniAppointment>>> get getAppointments async {
+  Future<Result<List<UniAppointment>>> getAppointments(String token) async {
     try {
-      final res = await http.get(
-        Uri.parse("http://localhost:8080/api/appointments/get"),
-      );
+      final res = await http
+          .get(
+            Uri.parse(TAppRoutes.getAppointmentsRoute),
+            headers: {
+              'Authorization': 'Bearer $token',
+              'Content-Type': 'application/json',
+            },
+          )
+          .timeout(const Duration(seconds: 10));
 
       if (res.statusCode == 200 && res.body.isNotEmpty) {
         List<dynamic> allAppointments = jsonDecode(res.body);
@@ -27,45 +34,128 @@ class UniAppointmentService {
             .map((app) => UniAppointment.fromJson(app))
             .toList();
         return Result.ok(appointments);
+      } else if (res.statusCode == 401 || res.statusCode == 403) {
+        return Result.error(UnauthorizedException());
+      } else if (res.statusCode == 404) {
+        return Result.error(AppointmentNotFoundException());
+      } else if (res.statusCode == 429) {
+        return Result.error(RateLimitException());
       } else {
-        return Result.error(Exception("Failed to fetch appointments"));
+        return Result.error(
+          ServerException("Failed with status : ${res.statusCode}"),
+        );
       }
+    } on SocketException {
+      return Result.error(NoInternetException());
+    } on FormatException {
+      return Result.error(DataParsingException());
+    } on TimeoutException {
+      return Result.error(NetworkTimeoutException());
     } on Exception catch (e) {
-      return Result.error(e);
+      return Result.error(UnknownServiceException(e.toString()));
+    } catch (e, stacktrace) {
+      return Result.error(UnknownServiceException(e.toString()));
     }
   }
 
-  Future<Result<void>> createAppointment(UniAppointment appointment) async {
+  Future<Result<UniAppointment>> createAppointment(
+    UniAppointment appointment,
+    String token,
+  ) async {
     try {
-      final res = await http.post(
-        Uri.parse("http://localhost:8080/api/appointments/create"),
-        body: appointment,
-      );
 
-      if (res.statusCode == 201 && res.body.isNotEmpty) {
-        return Result.ok(null);
+      final res = await http
+          .post(
+            Uri.parse(TAppRoutes.createAppointmentRoute),
+            body: jsonEncode({
+              'title': appointment.title,
+              'location': appointment.appointmentLocation,
+              'endTime': appointment.end.toIso8601String(),
+              'startTime': appointment.start.toIso8601String(),
+              'appointmentType': appointment.uniAppointmentType.name,
+              'appointmentScope': appointment.uniAppointmentScope.name,
+              'recurrenceRule': appointment.recurrence,
+            }),
+            headers: {
+              'Authorization': 'Bearer $token',
+              'Content-Type': 'application/json',
+            },
+          )
+          .timeout(const Duration(seconds: 10));
+      
+       if ((res.statusCode == 200 || res.statusCode == 201) &&
+          res.body.isNotEmpty) {
+        final Map<String, dynamic> app = jsonDecode(res.body);
+
+        return Result.ok(UniAppointment.fromJson(app));
+      } else if (res.statusCode == 400) {
+        return Result.error(BadRequestException());
+      } else if (res.statusCode == 401 || res.statusCode == 403) {
+        return Result.error(UnauthorizedException());
+      } else if (res.statusCode == 409) {
+        return Result.error(PromotionAlreadyExistsException());
+      } else if (res.statusCode == 429) {
+        return Result.error(RateLimitException());
+      } else if (res.statusCode >= 500) {
+        return Result.error(
+          ServerException("Server error with status code : ${res.statusCode}"),
+        );
       } else {
-        return Result.error(HttpException("Invalid response"));
+        return Result.error(CouldNotCreateAppointmentException());
       }
+    } on SocketException {
+      return Result.error(NoInternetException());
+    } on TimeoutException {
+      return Result.error(NetworkTimeoutException());
+    } on FormatException {
+      return Result.error(DataParsingException());
     } on Exception catch (e) {
-      return Result.error(e);
+      return Result.error(UnknownServiceException(e.toString()));
+    } catch (e, stacktrace) {
+     
+      return Result.error(UnknownServiceException(e.toString()));
     }
   }
 
-  Future<Result<void>> deleteAppointment(int appointmentId) async {
+  Future<Result<void>> deleteAppointment(
+    UniAppointment appointment,
+    String token,
+  ) async {
     try {
-      final res = await http.delete(
-        Uri.parse("http://localhost:8080/api/appointments/delete"),
-        body: appointmentId,
-      );
+      final res = await http
+          .delete(
+            Uri.parse(TAppRoutes.deleteAppointmentRoute),
+            body: jsonEncode(appointment),
+            headers: {
+              'Authorization': 'Bearer $token',
+              'Content-Type': 'application/json',
+            },
+          )
+          .timeout(const Duration(seconds: 10));
 
-      if (res.statusCode == 200) {
+      if (res.statusCode == 200 || res.statusCode == 204) {
         return Result.ok(null);
+      } else if (res.statusCode == 404) {
+        return Result.error(AppointmentNotFoundException());
+      } else if (res.statusCode == 401 || res.statusCode == 403) {
+        return Result.error(UnauthorizedException());
+      } else if (res.statusCode == 429) {
+        return Result.error(RateLimitException());
+      } else if (res.statusCode >= 500) {
+        return Result.error(
+          ServerException("Server error with status code : ${res.statusCode}"),
+        );
       } else {
-        return Result.error(Exception("Invalid response."));
+        return Result.error(CouldNotDeleteAppointmentException());
       }
+    } on SocketException {
+      return Result.error(NoInternetException());
+    } on TimeoutException {
+      return Result.error(NetworkTimeoutException());
     } on Exception catch (e) {
-      return Result.error(e);
+      return Result.error(UnknownServiceException(e.toString()));
+    } catch (e, stacktrace) {
+      return Result.error(UnknownServiceException(e.toString()));
     }
   }
 
@@ -73,10 +163,15 @@ class UniAppointmentService {
     String token,
   ) async {
     try {
-      final res = await http.post(
-        Uri.parse(TAppRoutes.getCurrentAppointmentsRoute),
-        headers: {'Authorization': 'Bearer $token'},
-      );
+      final res = await http
+          .post(
+            Uri.parse(TAppRoutes.getCurrentAppointmentsRoute),
+            headers: {
+              'Authorization': 'Bearer $token',
+              'Content-Type': 'application/json',
+            },
+          )
+          .timeout(const Duration(seconds: 10));
       if (res.statusCode == 200 && res.body.isNotEmpty) {
         List<dynamic> appointmentsJson = jsonDecode(res.body);
 
@@ -85,11 +180,27 @@ class UniAppointmentService {
             .toList();
 
         return Result.ok(currAppointments);
+      } else if (res.statusCode == 401 || res.statusCode == 403) {
+        return Result.error(UnauthorizedException());
+      } else if (res.statusCode == 404) {
+        return Result.error(AppointmentNotFoundException());
+      } else if (res.statusCode == 429) {
+        return Result.error(RateLimitException());
       } else {
-        return Result.error(Exception("Invalid response."));
+        return Result.error(
+          ServerException("Failed with status : ${res.statusCode}"),
+        );
       }
+    } on SocketException {
+      return Result.error(NoInternetException());
+    } on FormatException {
+      return Result.error(DataParsingException());
+    } on TimeoutException {
+      return Result.error(NetworkTimeoutException());
     } on Exception catch (e) {
-      return Result.error(e);
+      return Result.error(UnknownServiceException(e.toString()));
+    } catch (e, stacktrace) {
+      return Result.error(UnknownServiceException(e.toString()));
     }
   }
 
@@ -99,7 +210,10 @@ class UniAppointmentService {
     try {
       final res = await http.post(
         Uri.parse(TAppRoutes.getUpcomingSpecialEventsRoute),
-        headers: {'Authorization ': 'Bearer $token'},
+        headers: {
+          'Authorization ': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
       );
 
       if (res.statusCode == 200 && res.body.isNotEmpty) {
@@ -108,11 +222,27 @@ class UniAppointmentService {
             .map((app) => UniAppointment.fromJson(app))
             .toList();
         return Result.ok(specialEvents);
+      } else if (res.statusCode == 401 || res.statusCode == 403) {
+        return Result.error(UnauthorizedException());
+      } else if (res.statusCode == 404) {
+        return Result.error(AppointmentNotFoundException());
+      } else if (res.statusCode == 429) {
+        return Result.error(RateLimitException());
       } else {
-        return Result.error(Exception("Invalid response."));
+        return Result.error(
+          ServerException("Failed with status : ${res.statusCode}"),
+        );
       }
+    } on SocketException {
+      return Result.error(NoInternetException());
+    } on FormatException {
+      return Result.error(DataParsingException());
+    } on TimeoutException {
+      return Result.error(NetworkTimeoutException());
     } on Exception catch (e) {
-      return Result.error(e);
+      return Result.error(UnknownServiceException(e.toString()));
+    } catch (e, stacktrace) {
+      return Result.error(UnknownServiceException(e.toString()));
     }
   }
 }
