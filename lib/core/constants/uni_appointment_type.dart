66666571df857cf3985d51672001fb6db738b071
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-13 22:08:03 +0100

    added display name method for serializing puposes

--- lib/core/constants/uni_appointment_type.dart
+++ lib/core/constants/uni_appointment_type.dart
@@ -1,1 +1,19 @@
-enum UniAppointmentType { COURSE, TD, TP, TEST, EXAM, SPECIAL_EVENT }
+enum UniAppointmentType {
+  COURSE,
+  TD,
+  TP,
+  TEST,
+  EXAM,
+  SPECIAL_EVENT;
+
+  String get displayName {
+    return switch (this) {
+      UniAppointmentType.COURSE => "COURSE",
+      UniAppointmentType.TD => "TD",
+      UniAppointmentType.TP => "TP",
+      UniAppointmentType.SPECIAL_EVENT => "SPECIAL_EVENT",
+      UniAppointmentType.EXAM => "EXAM",
+      UniAppointmentType.TEST => "TEST",
+    };
+  }
+}
