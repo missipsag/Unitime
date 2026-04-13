ae4c7eaf65c1a0e105fbba1ccd2c1672829025e6
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-13 22:06:31 +0100

    added display name method for serializing purposes

--- lib/core/constants/promotion_level.dart
+++ lib/core/constants/promotion_level.dart
@@ -1,1 +1,23 @@
-enum PromotionLevel { L1, L2, L3, M1, M2, D1, D2, D3 }
+enum PromotionLevel {
+  L1,
+  L2,
+  L3,
+  M1,
+  M2,
+  D1,
+  D2,
+  D3;
+
+  String get displayName {
+    return switch (this) {
+      PromotionLevel.L1 => "L1",
+      PromotionLevel.L2 => "L2",
+      PromotionLevel.L3 => "L3",
+      PromotionLevel.M1 => "M1",
+      PromotionLevel.M2 => "M2",
+      PromotionLevel.D1 => "D1",
+      PromotionLevel.D2 => "D2",
+      PromotionLevel.D3 => "D3",
+    };
+  }
+}
