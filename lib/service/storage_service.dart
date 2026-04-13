67dc1df51ba23356ce88fa6679dededc76c96a08
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-13 22:50:31 +0100

    added robust error handling

--- lib/service/storage_service.dart
+++ lib/service/storage_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:unitime/core/utils/exceptions.dart';
 import 'package:unitime/core/utils/result.dart';
 
 class StorageService {
@@ -11,11 +12,14 @@ class StorageService {
   final _storage = FlutterSecureStorage(
     aOptions: AndroidOptions(),
     iOptions: IOSOptions(),
+    webOptions: WebOptions.defaultOptions,
   );
 
   Future<Result<void>> write(String key, String value) async {
     try {
-      await _storage.write(key: key, value: value);
+      await _storage
+          .write(key: key, value: value)
+          .catchError((e) => throw Exception(e));
       return Result.ok(null);
     } on Exception catch (e) {
       return Result.error(e);
@@ -24,7 +28,7 @@ class StorageService {
 
   Future<Result<void>> delete(String key) async {
     try {
-      await _storage.delete(key: key);
+      await _storage.delete(key: key).catchError((e) => throw Exception(e));
       return Result.ok(null);
     } on Exception catch (e) {
       return Result.error(e);
@@ -33,11 +37,15 @@ class StorageService {
 
   Future<Result<String>> read(String key) async {
     try {
-      final value = await _storage.read(key: key);
+      final value = await _storage
+          .read(key: key)
+          .catchError((e) => throw Exception(e));
       if (value != null) {
         return Result.ok(value);
       } else {
-        return Result.error(Exception("No value associated with $key."));
+        return Result.error(
+          NoValueAssociatedWithKeyException("No value associated with $key."),
+        );
       }
     } on Exception catch (e) {
       return Result.error(e);
