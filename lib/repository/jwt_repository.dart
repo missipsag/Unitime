c356d701bb847d43b43c086b851a33dbe0950adc
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-14 10:46:22 +0100

    added deleteToken method

--- lib/repository/jwt_repository.dart
+++ lib/repository/jwt_repository.dart
@@ -1,3 +1,4 @@
+import 'package:unitime/core/constants/env_config.dart';
 import 'package:unitime/core/utils/exceptions.dart';
 import 'package:unitime/core/utils/result.dart';
 import 'package:unitime/service/jwt_service.dart';
@@ -11,26 +12,29 @@ class JwtRepository {
   factory JwtRepository() => _shared;
 
   final _jwtService = JwtService();
-  final _storageSerice = StorageService();
-  static const String _authTokenKey = "jwt_token";
+  final _storageService = StorageService();
+  final String _authTokenKey = EnvConfig.authTokenKey;
 
   Future<Result<String?>> getToken() async {
-    final token =  await _storageSerice.read(_authTokenKey);
+    final token = await _storageService.read(_authTokenKey);
     try {
       switch (token) {
         case Ok<String>():
           return Result.ok(token.value);
 
         default:
-          return Result.error(CouldNotGetJWTToken());
+          return Result.error(CouldNotGetJWTException());
       }
     } on Exception catch (e) {
       return Result.error(e);
     }
   }
 
+  Future<Result<void>> deleteToken() async {
+    return await _storageService.delete(_authTokenKey);
+  }
+
   Future<Result<bool>> isTokenExpired(String token) async {
     return await _jwtService.isTokenExpired(token);
-
   }
 }
