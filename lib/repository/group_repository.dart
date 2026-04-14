7aee41bacd1578230de1c93e609e9e94c24e0d19
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-14 10:46:52 +0100

    added robust error handling

--- lib/repository/group_repository.dart
+++ lib/repository/group_repository.dart
@@ -1,6 +1,8 @@
+import 'package:unitime/core/constants/env_config.dart';
 import 'package:unitime/core/utils/result.dart';
 import 'package:unitime/data/group.dart';
 import 'package:unitime/service/group_service.dart';
+import 'package:unitime/service/storage_service.dart';
 
 class GroupRepository {
   static final GroupRepository _shared = GroupRepository._sharedInstance();
@@ -10,18 +12,44 @@ class GroupRepository {
   factory GroupRepository() => _shared;
 
   final _groupService = GroupService();
+  final _storageService = StorageService();
+  final String _authTokenKey = EnvConfig.authTokenKey;
 
-  Future<Result<Group>> createGroup(Group group, String token) async {
+  Future<Result<Group>> createGroup(
+    String groupName,
+    String accessCode,
+    int promotionId,
+  ) async {
     try {
-      return await _groupService.createGroup(group, token);
+      final token = await _storageService.read(_authTokenKey);
+
+      switch (token) {
+        case Ok<String>():
+          return await _groupService.createGroup(
+            groupName,
+            accessCode,
+            promotionId,
+            token.value,
+          );
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
     } on Exception catch (e) {
       return Result.error(e);
     }
   }
 
-  Future<Result<Group>> getGroup(String accessCode, String token) async {
+  Future<Result<Group>> getGroup(String accessCode) async {
     try {
-      return await _groupService.getGroup(accessCode, token);
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          return await _groupService.getGroup(accessCode, token.value);
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
     } on Exception catch (e) {
       return Result.error(e);
     }
