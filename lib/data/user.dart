dcac2bf0c63b86b80f965b24518f9fa5dd1ab8ea
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-14 10:59:17 +0100

    - Added roles attribute - Made group and promotion attribute nullable since at the beginning the use has none of them. - Edited fromJson method to treat nullable group and promotion and parse userRoles

--- lib/data/user.dart
+++ lib/data/user.dart
@@ -1,13 +1,17 @@
-
+import 'package:unitime/core/constants/user_role.dart';
+import 'package:unitime/data/group.dart';
+import 'package:unitime/data/promotion.dart';
 
 class User {
   final int id;
   final String email;
   final String firstName;
   final String lastName;
+  Group? group;
+  Promotion? promotion;
   final DateTime createdAt;
   final DateTime updatedAt;
-
+  final List<UserRole> roles;
   User({
     required this.id,
     required this.email,
@@ -15,6 +19,9 @@ class User {
     required this.lastName,
     required this.createdAt,
     required this.updatedAt,
+    required this.roles,
+    this.group,
+    this.promotion,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
@@ -24,16 +31,27 @@ class User {
         'email': String email,
         'firstName': String firstName,
         'lastName': String lastName,
-        'createdAt': DateTime createdAt,
-        'updatedAt': DateTime updatedAt,
+        'group': Map<String, dynamic>? group,
+        'promotion': Map<String, dynamic>? promotion,
+        'roles': List<dynamic> roles,
+        'createdAt': String createdAt,
+        'updatedAt': String updatedAt,
       } =>
         User(
           id: id,
           email: email,
           firstName: firstName,
           lastName: lastName,
-          createdAt: createdAt,
-          updatedAt: updatedAt,
+          group: group != null ? Group.fromJson(group) : null,
+          promotion: promotion != null ? Promotion.fromJson(promotion) : null,
+          createdAt: DateTime.parse(createdAt),
+          updatedAt: DateTime.parse(updatedAt),
+          roles: roles.map((e) {
+            return UserRole.values.firstWhere(
+              (role) => role.name.toLowerCase() == e.toString().toLowerCase(),
+              orElse: () => throw FormatException("Unknown role $e"),
+            );
+          }).toList(),
         ),
       _ => throw const FormatException("Failed to load user."),
     };
