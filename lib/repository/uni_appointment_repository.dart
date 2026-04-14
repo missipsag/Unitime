f81225c13369c69c50c4a8c98257e90f79e08d75
missipsag <ghernaoutmissipsa.pro@gmail.com>
2026-04-14 10:43:08 +0100

    added : - robust error handling, -getUpcommingSpecialEvents method - getCurrentAppointments

--- lib/repository/uni_appointment_repository.dart
+++ lib/repository/uni_appointment_repository.dart
@@ -1,5 +1,7 @@
+import 'package:unitime/core/constants/env_config.dart';
 import 'package:unitime/core/utils/result.dart';
 import 'package:unitime/data/uni_appointment.dart';
+import 'package:unitime/service/storage_service.dart';
 import 'package:unitime/service/uni_appointment_service.dart';
 
 class UniAppointmentRepository {
@@ -11,8 +13,77 @@ class UniAppointmentRepository {
   factory UniAppointmentRepository() => _shared;
 
   final UniAppointmentService _uniAppointmentService = UniAppointmentService();
+  final StorageService _storageService = StorageService();
+  final String _authTokenKey = EnvConfig.authTokenKey;
 
   Future<Result<List<UniAppointment>>> getAllAppointments() async {
-    return await _uniAppointmentService.getAppointments;
+    try {
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          return await _uniAppointmentService.getAppointments(token.value);
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
+  }
+
+  Future<Result<List<UniAppointment>>> getCurrentAppointments() async {
+    try {
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          return await _uniAppointmentService.getCurrentAppointments(
+            token.value,
+          );
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
+  }
+
+  Future<Result<List<UniAppointment>>> getUpcomingSpecialEvents() async {
+    try {
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          return await _uniAppointmentService.getUpcomingSpecialEvents(
+            token.value,
+          );
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
+  }
+
+  Future<Result<UniAppointment>> createAppointment(
+    UniAppointment appointment,
+  ) async {
+    try {
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          final res = await _uniAppointmentService.createAppointment(
+            appointment,
+            token.value,
+          );
+
+          return res;
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
   }
 }
--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -1,5 +1,7 @@
+import 'package:unitime/core/constants/env_config.dart';
 import 'package:unitime/core/utils/result.dart';
 import 'package:unitime/data/user.dart';
+import 'package:unitime/service/storage_service.dart';
 import 'package:unitime/service/user_service.dart';
 
 class UserRepository {
@@ -9,12 +11,67 @@ class UserRepository {
 
   factory UserRepository() => _shared;
   final UserService _userService = UserService();
+  final StorageService _storageService = StorageService();
+  final String _authTokenKey = EnvConfig.authTokenKey;
 
-  Future<Result<User>> getUser(String token) async {
-    return await _userService.getUser(token);
+  Future<Result<User>> getUser() async {
+    try {
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          final user = await _userService.getUser(token.value);
+          return user;
+        case Error<String>():
+          return Result.error(token.error);
+      }
+    } on Exception catch (e) {
+
+      return Result.error(e);
+    }
   }
 
-  Future<Result<User>> updateUser(User user, String token) async {
-    return await _userService.updateUser(user, token);
+  Future<Result<User>> updateUser(User user) async {
+    try {
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          return await _userService.updateUser(user, token.value);
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
+  }
+
+  Future<Result<void>> joinGroup(int groupId) async {
+    try {
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          return await _userService.joinGroup(groupId, token.value);
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
+  }
+
+  Future<Result<void>> joinPromotion(int promotionId) async {
+    try {
+      final token = await _storageService.read(_authTokenKey);
+      switch (token) {
+        case Ok<String>():
+          return await _userService.joinPromotion(promotionId, token.value);
+
+        case Error<String>():
+          return Result.error(token.error);
+      }
+    } on Exception catch (e) {
+      return Result.error(e);
+    }
   }
 }
